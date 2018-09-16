6369fd42f90f6287674d4707900be4b0aa27987f
unknown <f.l.spelling@gmail.com>
2018-09-16 12:16:10 -0300

    implementação compoenentes home, header, about

--- meat-app/src/app/app.module.ts
+++ meat-app/src/app/app.module.ts
@@ -2,19 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
-
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
+import { ROUTES } from './app.routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
